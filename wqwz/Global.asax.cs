ac6626e19626d8728743da75c67f72d4e66a4056
gyn7561 <200804632@qq.com>
2015-04-09 09:31:27 +0800

    去除BootstrapMVC
    有性能问题

--- wqwz/Global.asax.cs
+++ wqwz/Global.asax.cs
@@ -5,8 +5,7 @@ using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
-using System.Web.Routing;
-using TwitterBootstrapMVC;
+using System.Web.Routing; 
 
 namespace wqwz
 {
