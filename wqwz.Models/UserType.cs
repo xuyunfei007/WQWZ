af176fcc3f64ca88e9798bb3a0f1b5662676661f
gyn7561 <200804632@qq.com>
2015-04-10 16:59:13 +0800

    小更改

--- wqwz.Models/UserType.cs
+++ wqwz.Models/UserType.cs
@@ -2,13 +2,18 @@
 namespace wqwz.Models
 {
     using System;
+    using System.ComponentModel;
     
     [Flags]
     public enum UserType : int
     {
+        [Description("普通管理员")]
         Admin = 0,
+        [Description("超级管理员")]
         SuperAdmin = 1,
+        [Description("普通用户")]
         User = 2,
+        [Description("高级用户")]
         AdvanceUser = 3
     }
 }
